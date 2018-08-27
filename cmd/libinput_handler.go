92282c44eb08c659b95b951c8c4b3489ff19e5fd
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2018-08-27 19:17:18 -0400

    Improve IsValidDebugEvent function

--- cmd/libinput_handler.go
+++ cmd/libinput_handler.go
@@ -2,19 +2,16 @@ package main
 
 import "github.com/vitakras/libinput-gestures/pkg/libinput"
 
-// SupportedActions list of supported libinput events
-var SupportedActions = []string{
-	"GESTURE_SWIPE_UPDATE",
-	"GESTURE_PINCH_UPDATE",
+// SupportedActionsMap list of supported libinput events
+var SupportedActionsMap = map[string]bool{
+	"GESTURE_SWIPE_START":  true,
+	"GESTURE_SWIPE_UPDATE": true,
+	"GESTURE_PINCH_START":  true,
+	"GESTURE_PINCH_UPDATE": true,
 }
 
 // IsValidDebugEvent return true of false whether the debug event is valid
 func IsValidDebugEvent(debugEvent *libinput.DebugEvent) bool {
-	for _, actionType := range SupportedActions {
-		if actionType == debugEvent.Action {
-			return true
-		}
-	}
-
-	return false
+	_, supported := SupportedActionsMap[debugEvent.Action]
+	return supported
 }
